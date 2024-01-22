eceaaac3b0d0b55a1a8f3c1bb6f0cb6cbed5ae12
Kinkandju <ferretsphilosophy@gmail.com>
2024-01-22 17:07:58 +0300

    refactor: add comments to 05-merge-styles

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -6,22 +6,30 @@ async function mergeStyles() {
   const bundleFile = './05-merge-styles/project-dist/bundle.css';
 
   try {
+    /* read the contents of the styles folder */
     const files = await readdir(stylesDir);
 
+    /* create an array to store the contents of all style files */
     const stylesArray = [];
 
+    /* read each style file in the styles folder */
     for (const file of files) {
       const filePath = join(stylesDir, file);
 
+      /* check if the style file has the .css extension */
       if (file.endsWith('.css')) {
+        /* read the contents of the file */
         const fileData = await readFile(filePath, 'utf8');
 
+        /* add the contents to the array */
         stylesArray.push(fileData);
       }
     }
 
+    /* combine all styles into one large line separated by a line break */
     const combinedStyles = stylesArray.join('\n');
 
+    /* write the combined styles to the bundle.css file */
     await writeFile(bundleFile, combinedStyles, 'utf8');
 
     console.log('The style file has been successfully merged!');
@@ -31,4 +39,3 @@ async function mergeStyles() {
 }
 
 mergeStyles();
-  
