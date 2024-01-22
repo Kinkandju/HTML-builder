977c3b121892503c191e6b349d8538f0bce44e47
Kinkandju <ferretsphilosophy@gmail.com>
2024-01-22 17:23:35 +0300

    refactor: add comments to 06-build-page

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -7,12 +7,19 @@ async function buildPage() {
   const distDir = './06-build-page/project-dist';
 
   try {
+    /* create the project-dist folder, if it does not already exist */
     await mkdir(distDir, { recursive: true });
 
+    /* read the contents of the template */
     const templateContent = await readFile(templateFile, 'utf8');
+
+    /* get all the tags in the template */
     const tags = findTags(templateContent);
+
+    /* replace tags with component contents */
     const pageContent = await replaceTags(templateContent, tags, componentsDir);
 
+    /* write the modified template to a file index.html to the project-dist folder */
     await writeFile(join(distDir, 'index.html'), pageContent, 'utf8');
 
     console.log('The page has been successfully builded!');
@@ -22,19 +29,34 @@ async function buildPage() {
 }
 
 function findTags(templateContent) {
+  /* create a regular expression to search for tags in the template */
   const regex = /\{\{([^{}]+)\}\}/g;
+
+  /* look for all tags in the template */
   const tags = templateContent.match(regex);
+
+  /* if tags are found -> return a formatted array of tags,
+  otherwise -> return an empty array */
   return tags ? tags.map(tag => tag.slice(2, -2).trim()) : [];
+  /* - tag.slice(2, -2) cuts the first two and last two characters in each element.
+  The tags have the format {{ tag }}, so remove the curly braces.
+  - trim() removes spaces at the edges of the string to get a clean tag value. */
 }
 
 async function replaceTags(templateContent, tags, componentsDir) {
+  /* create a variable to store the modified page content */
   let pageContent = templateContent;
 
+  /* iterate on each tag */
   for (const tag of tags) {
+    /* form the path to the component file */
     const componentFile = join(componentsDir, `${tag}.html`);
 
     try {
+      /* read the contents of the component */
       const componentContent = await readFile(componentFile, 'utf8');
+
+      /* replace the tag in the page content with the content of the component */
       pageContent = pageContent.replace(new RegExp(`{{\\s*${tag}\\s*}}`, 'g'), componentContent);
     } catch (error) {
       console.error(`Error reading the component ${componentFile}:`, error);
