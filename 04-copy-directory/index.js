f11358234c8f48f2becca3972d1427af21e7b46b
Kinkandju <ferretsphilosophy@gmail.com>
2024-01-22 14:13:59 +0300

    feat: add solution for 04-read-file

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,44 @@
+const { mkdir, readdir, copyFile, stat } = require('fs/promises');
+const { join } = require('path');
+
+async function copyDir() {
+  const sourceDir = './04-copy-directory/files';
+  const destinationDir = './04-copy-directory/files-copy';
+  
+  try {
+    const dirExists = await checkIfDirectoryExists(destinationDir);
+    if (!dirExists) {
+      await mkdir(destinationDir, { recursive: true });
+    }
+  
+    const files = await readdir(sourceDir);
+  
+    for (const file of files) {
+      const sourceFilePath = join(sourceDir, file);
+      const destinationFilePath = join(destinationDir, file);
+  
+      await copyFile(sourceFilePath, destinationFilePath);
+    }
+  
+    console.log('The folder has been copied successfully!');
+  } catch (error) {
+    console.error('Error copying folder:', error);
+  }
+}
+  
+async function checkIfDirectoryExists(dirPath) {
+  try {
+    const dirStats = await stat(dirPath);
+
+    return dirStats.isDirectory();
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      return false;
+    }
+
+    throw error;
+  }
+}
+
+copyDir();
+  
