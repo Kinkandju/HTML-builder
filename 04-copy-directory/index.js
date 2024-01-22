c83dd7c7fc6c4c9f405a6c52e3f8002473836e4c
Kinkandju <ferretsphilosophy@gmail.com>
2024-01-22 16:58:44 +0300

    refactor: add comments to 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,28 +4,31 @@ const { join } = require('path');
 async function copyDir() {
   const sourceDir = './04-copy-directory/files';
   const destinationDir = './04-copy-directory/files-copy';
-  
+
   try {
+    /* check if the files-copy folder exists, and if not, then create it */
     const dirExists = await checkIfDirectoryExists(destinationDir);
     if (!dirExists) {
       await mkdir(destinationDir, { recursive: true });
     }
-  
+
+    /* read the contents of the files folder */
     const files = await readdir(sourceDir);
-  
+
+    /* copy each file from the files folder to the files-copy folder */
     for (const file of files) {
       const sourceFilePath = join(sourceDir, file);
       const destinationFilePath = join(destinationDir, file);
   
       await copyFile(sourceFilePath, destinationFilePath);
     }
-  
+
     console.log('The folder has been copied successfully!');
   } catch (error) {
     console.error('Error copying folder:', error);
   }
 }
-  
+
 async function checkIfDirectoryExists(dirPath) {
   try {
     const dirStats = await stat(dirPath);
@@ -41,4 +44,3 @@ async function checkIfDirectoryExists(dirPath) {
 }
 
 copyDir();
-  
